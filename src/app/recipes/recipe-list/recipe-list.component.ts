241daeb194f30efd559d5a8b27ba395077bdf9e7
Danilo Caruso <danilo.caruso.ge@gmail.com>
2017-05-07 22:32:30 +0200

    Modified recipeListComponent using Recipe model class, declared and populated an array of Recipe and made a Loop on the view for display those mock data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from './../recipe.model';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.less']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+      new Recipe('Melanzane allla Parmigiana', 'Melanzane alla parmigiana Howto', 'http://www.tuttogreen.it/wp-content/uploads/2016/08/Schermata-08-2457611-alle-11.22.20-400x250.png')
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
